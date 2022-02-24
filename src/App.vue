20bb36d4cbb2b3d2dae5ef7650286e350a420774
Joss Dixon <josscdixon@gmail.com>
2022-02-24 21:21:07 +0000

    here we go

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
   <h1>Wordle Clone</h1>
+  <Guess :word="word"/>
 </template>
 
 <script>
+import Guess from './components/Guess.vue'
 
 export default {
+  name: 'App',
+  components: { Guess },
+  data() {
+    return {
+      word: [''],
+    }
+  },
+  mounted() {
+    function sample(array) {
+      return array[Math.floor ( Math.random() * array.length )]
+    }
+    const words = ['words', 'flame'];
+    this.word = sample(words).split('');
+    console.log(this.word);
+  }
 }
 </script>
 
---
+++ src/components/Guess.vue
@@ -0,0 +1,65 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" v-model="first">
+    <input type="text" v-model="second">
+    <input type="text" v-model="third">
+    <input type="text" v-model="fourth">
+    <input type="text" v-model="fifth">
+    <input type="submit">
+    <div v-if="entryError" class="error">{{ entryError }}</div>
+    <div v-if="guessResponse" class="error">{{ guessResponse }}</div>
+  </form>
+
+</template>
+
+<script>
+
+export default {
+  props: ['word'],
+  data() {
+    return {
+      first: '',
+      second: '',
+      third: '',
+      fourth: '',
+      fifth: '',
+      entryError: '',
+      guessResponse: ''
+    }
+  },
+  methods: {
+    handleSubmit() {
+      this.guessResponse = '';
+      const guessJoined = [this.first, this.second, this.third, this.fourth, this.fifth];
+      this.entryError = guessJoined.includes('') ? 'Word is too short' : '';
+
+      if (!this.entryError) {
+        while (this.guessResponse === '') {
+          console.log(this.word);
+          console.log(guessJoined);
+          // if (this.guessJoined.join() === this.word.join()) {
+          //   return this.guessResponse = 'Spot on!'
+          // }
+          this.word.forEach((letter) => {
+            if (guessJoined.includes(letter)) {
+              return this.guessResponse = 'Good guess!'
+            } else {
+              return this.guessResponse = 'Try again'
+            }
+          })
+        }
+      }
+    }
+  }
+}
+
+</script>
+
+<style>
+  input[type=text] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+  }
+</style>
